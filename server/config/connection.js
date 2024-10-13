42cff6cf2731bdaa23f113aa5d0df6af28835800
Jocephuss <rowlandjosiah14@gmail.com>
2024-10-12 20:21:39 -0600

    "Adjusted connection.js"

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
-
 require("dotenv").config();
 
-mongoose.connect(mongoose.connect(process.env.MONGODB_URI, {}));
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+  });
 
 module.exports = mongoose.connection;
