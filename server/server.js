2bcf4d47b2b8f1e380d6573fba4d81facdbf43d8
Jocephuss <rowlandjosiah14@gmail.com>
2024-10-12 19:57:48 -0600

    "Adjusted server.js"

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+  console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+});
 const app = express();
 const server = new ApolloServer({
   typeDefs,
